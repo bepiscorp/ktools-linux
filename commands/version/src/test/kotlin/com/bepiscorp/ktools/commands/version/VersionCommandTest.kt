b7b7411fe18eb37e65784089b8ccbdf8d41f9d69
Mikhail Gostev <gostev.mike@gmail.com>
2025-08-30 10:18:56 +0300

    build: fix failing build

--- commands/version/src/test/kotlin/com/bepiscorp/ktools/commands/version/VersionCommandTest.kt
+++ commands/version/src/test/kotlin/com/bepiscorp/ktools/commands/version/VersionCommandTest.kt
@@ -1,6 +1,7 @@
 package com.bepiscorp.ktools.commands.version
 
 import com.bepiscorp.ktools.core.di.coreModule
+import com.bepiscorp.ktools.core.BuildInfo
 import com.github.ajalt.clikt.testing.test
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.string.shouldContain
@@ -11,6 +12,6 @@ class VersionCommandTest : StringSpec({
 
     "prints version" {
         val result = VersionCommand().test("")
-        result.stdout shouldContain System.getProperty("ktools.version", "0.0.0")
+        result.stdout shouldContain BuildInfo.version
     }
 })
